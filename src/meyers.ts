fe05de07aceeb4c9c4ff575cac0b52e555e644ca
Logan Kearsley <chronosurfer@gmail.com>
2020-04-01 15:12:05 -0600

    Fix some bugs, remove unnecessary zeroing, add some features

--- src/meyers.ts
+++ src/meyers.ts
@@ -3,19 +3,20 @@ import { Indexable, Vec4 } from './types';
 function * diff_rec<T extends Indexable>(
   xs: T, i: number, N: number,
   ys: T, j: number, M: number,
-): Generator<Vec4> {
+): Generator<Vec4, undefined> {
   const stack: Vec4[] = [];
+
   let Z = 2 * Math.min(N, M) + 2;
   let c = new Uint32Array(Z);
   let d = new Uint32Array(Z);
+  let [D, x, y, u, v] = [0, 0, 0, 0, 0];
+
   for (;;) {
     Z_block: {
       if (N > 0 && M > 0) {
-        let [D, x, y, u, v] = [0, 0, 0, 0, 0];
-
         const L = N + M;
         const delta = N - M;
-        const hmax = L >>> 1 + (L&1);
+        const hmax = (L >>> 1) + (L&1);
         hloop: for (let h = 0; h <= hmax; h++) {
           const kmin = 2 * Math.max(0, h - M) - h;
           const kmax = h - 2 * Math.max(0, h - N);
@@ -26,11 +27,11 @@ function * diff_rec<T extends Indexable>(
             const ckm = c[(Z + k - 1) % Z];
             u = (k === -h || (k !== h && ckm < ckp)) ? ckp : ckm + 1;
             v = u - k;
-            [x,y] = [u,v];
+            [x, y] = [u, v];
             while (u < N && v < M && xs[i + u] === ys[j + v]) u++, v++;
             c[(Z + k) % Z] = u;
             const z = delta - k;
-            if ((L&1) === 1 && z >= (1 - h) && z <= (h - 1) && u + d[(Z + z) % Z] >= N) {
+            if ((L&1) === 1 && z > -h && z < h && u + d[(Z + z) % Z] >= N) {
               D = 2 * h - 1;
               break hloop;
             }
@@ -63,11 +64,11 @@ function * diff_rec<T extends Indexable>(
           if (N > 0 && M > 0) break Z_block;
         }
       }
-
+        
       if (M > N) {
-        yield [i + N, i + N, j + N, j + M - N];
+        yield [i + N, i + N, j + N, j + N + M];
       } else if (M < N) {
-        yield [i + M, i + N, 0, 0];
+        yield [i + M, i + M + N, 0, 0];
       }
 
       if (stack.length === 0) return;
@@ -75,19 +76,60 @@ function * diff_rec<T extends Indexable>(
     }
 
     Z = 2 * Math.min(N, M) + 2;
-    c[1] = d[1] = 0;
+    D = c[1] = d[1] = 0;
   }
 }
 
-// TODO: merge adjacent records
-export function diff<T extends Indexable>(xs: T, ys: T) {
+export function * diff<T extends Indexable>(xs: T, ys: T) {
   let i = 0; // skip common prefix
-  while (xs[i] === ys[i]) i++;
-
-  // skip common suffix
   let N = xs.length;
   let M = ys.length;
-  while (xs[--N] === ys[--M]);
+  while (i < N && i < M && xs[i] === ys[i]) i++;
 
-  return diff_rec(xs, i, N + 1 - i, ys, i, M + 1 - i);
+  // skip common suffix
+  while (N > i && M > i && xs[--N] === ys[--M]);
+
+  const iter = diff_rec(xs, i, N + 1 - i, ys, i, M + 1 - i);
+  let last = iter.next().value as Vec4;
+  if (!last) return;
+
+  for (const next of iter) {
+    const [nds, nde, nis, nie] = next;
+    if (nds === nde) { // this is an insert
+      if (nis === last[3]) {
+        // Multiple adjacent inserts in a row
+        last[3] = nie;
+      } else if (last[2] === last[3] && nds >= last[0] && nds <= last[1]) {
+        // Last record was a deletion, & this insert replaces it
+        last[2] = nis;
+        last[3] = nie;
+      } else {
+        yield last;
+        last = next;
+      }
+    } else if (nds === last[1]) {
+      // Multiple adjacent deletions in a row
+      last[1] = nde;
+    } else {
+      yield last;
+      last = next;
+    }
+  }
+  yield last;
+}
+
+export function * calcPatch<T extends Indexable>(xs: T, ys: T): Generator<[number, number, T]> {
+  for (const [ds, de, is, ie] of diff(xs, ys)) {
+    yield [ds, de, ys.slice(is, ie) as T];
+  }
+}
+
+export function * applyPatch<T extends Indexable>(xs: T, patch: Iterable<[number, number, T]>): Generator<T> {
+  let i = 0;
+  for (const [ds, de, ins] of patch) {
+    if (i < ds) yield xs.slice(i, ds) as T;
+    if (ins.length > 0) yield ins;
+    i = de;
+  }
+  if (i < xs.length) yield xs.slice(i) as T;
 }
--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,14 @@
-//import { lcs, Indexable, Vec4 } from './hirschberg';
-import { diff } from './meyers';
+import { lcs, Indexable, Vec4 } from './hirschberg';
+import { calcPatch, applyPatch, diff } from './meyers';
+//import { diff } from './meyers';
 
-//function extract(xs: Indexable, indices: Vec4[]) {
-//  return indices.map(([s, e]) => xs.slice(s, e)).join('');
-//}
+function extract(xs: Indexable, indices: Vec4[]) {
+  return indices.map(([s, e]) => xs.slice(s, e)).join('');
+}
 
 const tests = [
   ['preabmcdpost', 'prezxmywpost', 'prempost'],
-  /*['abcfboopqxyz', 'abcgbooprxyz', 'abcboopxyz'],
+  ['abcfboopqxyz', 'abcgbooprxyz', 'abcboopxyz'],
   ['', '', ''],
   ['a', '', ''],
   ['', 'b', ''],
@@ -17,15 +18,15 @@ const tests = [
   ['abc', 'bc', 'bc'],
   ['abcde', 'zbodf', 'bd'],
   ['aa','aaaa', 'aa'],
-  ['GTCGTTCGGAATGCCGTTGCTCTGTAAA', 'ACCGGTCGAGTGCGCGGAAGCCGGCCGAA', 'GTCGTCGGAAGCCGGCCGAA'],*/
+  ['GTCGTTCGGAATGCCGTTGCTCTGTAAA', 'ACCGGTCGAGTGCGCGGAAGCCGGCCGAA', 'GTCGTCGGAAGCCGGCCGAA'],
 ];
 
 for (const [xs, ys, ans] of tests) {
-  /*const arr = [...lcs(xs, ys)];
-  //console.log(arr);
-  const res = extract(xs, arr);
-  if (res !== ans) console.error('error:', xs, ys, res, ans, res.length === ans.length);
-  else console.log('success:', xs, ys, res);
-  */
-  console.log(xs, ys, ans, [...diff(xs, ys)]);
+  const common = extract(xs, [...lcs(xs, ys)]);
+  const es1 = [...diff(xs, ys)];
+  //console.log(xs, ys, JSON.stringify(es1));
+  const es2 = [...calcPatch(xs, ys)];
+  const edit = [...applyPatch(xs, es2)].join('');
+  if (common !== ans) console.error('lcs error:', xs, ys, ans, common);
+  if (edit !== ys) console.error('dif error:', xs, ys, edit, es1, es2);
 }
